5f041da80379007dbbbc03f37a39ce4077cbe7ab
yosrimhamdi <bavary1515@gmail.com>
2021-05-12 15:53:55 +0100

    connecting to a local mongodb database.

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test', {
+  useNewUrlParser: true,
+  useFindAndModify: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(3000);
