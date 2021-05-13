2517a24e3263a84d6e42a7208a4f35361f091079
yosrimhamdi <bavary1515@gmail.com>
2021-05-13 01:22:15 +0100

    excuting the actual payment.

--- controllers/success.js
+++ controllers/success.js
@@ -1,3 +1,28 @@
+const paypal = require('paypal-rest-sdk');
+
 module.exports = (req, res) => {
-  res.send('success');
+  const execute_payment_json = {
+    'payer_id': req.query.PayerID,
+    'transactions': [
+      {
+        'amount': {
+          'currency': 'USD',
+          'total': '500',
+        },
+      },
+    ],
+  };
+
+  paypal.payment.execute(
+    req.query.paymentId,
+    execute_payment_json,
+    function (error, payment) {
+      if (error) {
+        throw error;
+      } else {
+        console.log(payment);
+        res.send('Success');
+      }
+    },
+  );
 };
